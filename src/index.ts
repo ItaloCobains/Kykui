f4ef52ab4803b9e2bf5118f6af80a41f58d4d3fd
ItaloCobains <monokatchaleger@gmail.com>
2022-10-25 17:34:07 -0300

    fix: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import { App } from './app';
+import './lib/db';
+import express from 'express';
+import userRoutes from './routes/user.route';
 
-new App().getApp();
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+app.use(express.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.text({ type: 'text/html' }));
+
+app.get('/', async (req, res) => {
+  res.json({ message: 'Home' });
+});
+app.use('/user', userRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
