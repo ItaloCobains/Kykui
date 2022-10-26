65ffb995ad978edc4db8eb6f62d9ba53da9f5e33
ItaloCobains <monokatchaleger@gmail.com>
2022-10-26 10:39:20 -0300

    feat: add more middlewares

--- src/middlewares/express.middleware.ts
+++ src/middlewares/express.middleware.ts
@@ -1,5 +1,9 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import helmet from 'helmet';
 import userRoutes from '../routes/user.route';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -7,6 +11,10 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
 
 app.get('/', async (req, res) => {
   res.json({ message: 'Home' });
