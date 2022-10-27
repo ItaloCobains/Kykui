6e907b9d3333a33f660843a743fa7415a00885b2
ItaloCobains <monokatchaleger@gmail.com>
2022-10-27 14:36:02 -0300

    fix: user interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,16 +1,16 @@
 /* Defining the interface for the User model. */
 export interface UserInterface {
     _id?: any | string;
-    name?: string;
-    email?: string;
-    password?: string;
-    avatar?: string;
+    name: string;
+    email: string;
+    password: string;
+    avatar: string;
 }
 
-export interface MessageUser extends UserInterface {
-    // _id: any;
-    // name: string;
-    // avatar: string;
+export interface MessageUser {
+    _id: string;
+    name: string;
+    avatar: string;
     lastMessage: string | null;
     dateLastMessage: Date | null | undefined;
 }
